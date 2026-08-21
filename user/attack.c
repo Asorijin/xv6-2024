c6d75dbd7dc498f313d9d109ed7d9b77abd23fb8
Asorijin <865531792@qq.com>
2026-08-22 01:53:10 +0800

    syscall: implement attack (recover secret) and fix LAB setting

--- user/attack.c
+++ user/attack.c
@@ -6,16 +6,16 @@
 int
 main(int argc, char *argv[])
 {
-  // your code here.  you should write the secret to fd 2 using write
-  // (e.g., write(2, secret, 8)
-  if(argc != 1){
-    printf("Usage: secret the-secret\n");
-    exit(1);
-  }
-  char *end = sbrk(PGSIZE*200);
-  for(int i=1;i<200;i++){
-    printf("**%s**",end+32);
-    end = end + PGSIZE;
+  char *end = sbrk(PGSIZE * 128);
+  for(char *p = end; p < end + PGSIZE * 128; p += PGSIZE){
+    for(int o = 0; o < PGSIZE - 32; o++){
+      if(p[o] == 's' && p[o+1] == 'e' && p[o+2] == 'c' && p[o+3] == 'r' &&
+         p[o+4] == 'e' && p[o+5] == 't' && p[o+6] == ' ' && p[o+7] == 'p' &&
+         p[o+8] == 'w' && p[o+9] == ' ' && p[o+10] == 'i' && p[o+11] == 's'){
+        write(2, p + o + 14, 8);
+        exit(0);
+      }
+    }
   }
   exit(1);
 }
